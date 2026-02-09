9f229e8cda204406c1b3331e9d0f4e8335bdd106
Rupa <rupadevik684@gmail.com>
2026-02-09 17:31:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const navlist = document.getElementById('navlist');
+const hamburger = document.getElementById('hamburger');
+
+// click Event
+
+hamburger .addEventListener('click', () =>{
+
+    navlist.classList.toggle('navlist-active');
+
+})
